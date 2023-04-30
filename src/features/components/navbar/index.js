8d66fd5b3747b619d19dbd44560b68e6647ee928
Ezpzx <ezraabednego21@gmail.com>
2023-04-30 10:26:37 +0700

    test

--- src/features/components/navbar/index.js
+++ src/features/components/navbar/index.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  //asede
   return (
     <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div class="relative flex items-center justify-between">
